458d8d69cd90e72e350200839ad9ce0b64f199b5
RickyDR <160418067+RickyDR@users.noreply.github.com>
2024-02-21 23:36:04 -0500

    Add files via upload

---
+++ P1HW1_RiosRicky.py
@@ -0,0 +1,24 @@
+#Your Name
+
+#Date
+
+#Assignment Name
+
+#A brief description of the project
+
+print("Enter integer:",end=' ')
+user_num=int(input())
+print(user_num)
+print("You entered:",user_num)
+user_num_squared=int(user_num*user_num)
+print(user_num, 'squared is',user_num_squared,'!!')
+user_num_cubed=int(user_num*user_num*user_num)
+print('and',user_num,'cubed is',user_num_cubed,'!!')
+print("Enter integer:",end=' ')
+user_num_2=int(input())
+print(user_num_2)
+user_num_sum_add=int(user_num+user_num_2)
+print(user_num,'+',user_num_2,'is',user_num_sum_add)
+user_num_sum_mult=int(user_num*user_num_2)
+print(user_num,'*',user_num_2,'is',user_num_sum_mult)
+
